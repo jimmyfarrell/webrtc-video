f451a309a1dda1280fc430259a844df176748318
jimmyfarrell <jfarrell22@gmail.com>
2015-09-30 15:06:48 -0400

    Basic video chat works

--- client/pre-build/home/home.controller.js
+++ client/pre-build/home/home.controller.js
@@ -1,5 +1,42 @@
-app.controller('HomeController', function($scope, $http) {
-  
-  $scope.msgFromScope = "...And I'm a message from the HomeController scope, just so you know that I work!";
+app.controller('HomeController', function($scope, $http, $sce) {
+
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    navigator.getUserMedia({ audio: true, video: true }, function(stream) {
+        myStream = stream;
+    }, function(err) {
+        console.log('There was an error. Surprise surprise.');
+    });
+
+    var myStream;
+
+    $scope.callId = '';
+    $scope.myId = '';
+    $scope.video = '';
+
+    var peer = new Peer({key: 's5lw9do7zht1emi'});
+
+    peer.on('open', function(id) {
+        $scope.myId = id;
+        $scope.$digest();
+    });
+
+    peer.on('call', function(call) {
+        call.answer(myStream)
+        showVideo(call);
+    });
+
+    $scope.makeCall = function(callId) {
+        var call = peer.call(callId, myStream);
+        showVideo(call);
+    };
+
+    var showVideo = function(call) {
+        console.log('peer.connections', peer.connections);
+        call.on('stream', function(stream) {
+            $scope.video = $sce.trustAsResourceUrl(URL.createObjectURL(stream));
+            $scope.$digest();
+        });
+    };
 
 });
